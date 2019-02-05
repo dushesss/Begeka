a34acdb4ad2bb084a577a8072719e387f0895923
dushesss <dushessssssss45@gmail.com>
2019-02-05 09:05:37 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Lab2SoftWare
+{
+    partial class FormLab2
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxContent = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxContent
+            // 
+            this.lbxContent.FormattingEnabled = true;
+            this.lbxContent.Items.AddRange(new object[] {
+            "Раз, Два. Три! Четыре; Пять$",
+            "Туда\' Сюда . Куда\"\"б",
+            ",./?\'\";:!@#$%^&*()"});
+            this.lbxContent.Location = new System.Drawing.Point(13, 13);
+            this.lbxContent.Name = "lbxContent";
+            this.lbxContent.Size = new System.Drawing.Size(329, 277);
+            this.lbxContent.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(13, 297);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Пуск";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lResult
+            // 
+            this.lResult.AutoSize = true;
+            this.lResult.Location = new System.Drawing.Point(94, 302);
+            this.lResult.Name = "lResult";
+            this.lResult.Size = new System.Drawing.Size(59, 13);
+            this.lResult.TabIndex = 2;
+            this.lResult.Text = "Результат";
+            // 
+            // FormLab2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 328);
+            this.Controls.Add(this.lResult);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbxContent);
+            this.Name = "FormLab2";
+            this.Text = "Лабораторная работа 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxContent;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2SoftWare
+{
+    public partial class FormLab2 : Form
+    {
+        public FormLab2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int index = lbxContent.SelectedIndex;
+            string str = (string)lbxContent.Items[index];
+            int len = str.Length;
+            int count = 0;
+            for(int i = 0; i < len; i++) 
+            {
+                if (str[i] == ',' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == ';' || str[i] == ':')
+                    count++;
+            }
+            lResult.Text = "Количество знаков препинания = " + count.ToString();
+        }
+    }
+}
